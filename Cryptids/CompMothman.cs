ebaf4bf87558e25fe18045bd8c02346608b16a5b
makentake <elishafast@gmail.com>
2025-01-13 23:16:24 -0800

    starting on the mothman comp

--- Cryptids/CompMothman.cs
+++ Cryptids/CompMothman.cs
@@ -9,5 +9,20 @@ namespace Cryptids
 {
     public class CompMothman : ThingComp
     {
+        public MothmanState state;
+
+        // may be required?
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref state, "state", MothmanState.Wander);
+        }
+    }
+
+    public enum MothmanState
+    {
+        Wander,
+        Hunt,
+        Flee
     }
 }
--- Cryptids/ThinkNode_ConditionalMothmanState.cs
+++ Cryptids/ThinkNode_ConditionalMothmanState.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
 
 namespace Cryptids
 {
-    internal class ThinkNode_ConditionalMothmanState
+    public class ThinkNode_ConditionalMothmanState : ThinkNode_Conditional
     {
+        public MothmanState state;
+
+        protected override bool Satisfied(Pawn pawn)
+        {
+            var compMothman = pawn.TryGetComp<CompMothman>();
+
+            if (compMothman != null)
+            {
+                return compMothman.state == state;
+            }
+
+            return false;
+        }
     }
 }
